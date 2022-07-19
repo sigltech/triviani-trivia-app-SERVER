719d4adda6e382a527e6cd3e450da31be00fad92
Summira Hussain <summira508411@gmail.com>
2022-07-19 15:47:30 +0100

    work

--- dbConfig/dev_seeds.sql
+++ dbConfig/dev_seeds.sql
@@ -1,1 +1,5 @@
-m
+INSERT INTO users (username, score) 
+VALUES
+('sumshuss508', 14),
+('aaron9999', 10),
+('richardS9', 11);
--- dbConfig/seedDev.js
+++ dbConfig/seedDev.js
@@ -1,1 +1,6 @@
-fg
+const db = require('./init');
+const fs = require('fs');
+
+const seeds = fs.readFileSync(__dirname + '/dev_seeds.sql').toString();
+
+db.query(seeds, () => console.log('Dev database seeded'));
