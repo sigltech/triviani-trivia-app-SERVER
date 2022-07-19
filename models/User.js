920a6adbde71a6f116121af23061a255fee61d40
Aaron Smith <2021manifest@gmail.com>
2022-07-19 15:57:50 +0100

    basic users model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,27 @@
-h
+const db = require("../dbConfig/init");
+
+
+module.exports = class User {
+
+    constructor(data) {
+        this.id = data.id
+        this.username = data.username
+        this.score = data.score
+
+    }
+
+    static get all() {
+        return new Promise(async (res, rej) => {
+            try {
+
+                // const result = await db.query('SELECT * FROM users;')
+                // const users = result.rows.map(a => ({ id: a.id, name: a.name }))
+                // console.log(users)
+                // res(users)
+
+            } catch (err) {
+                // rej('Error!' + err)
+            }
+        })
+    }
+}
