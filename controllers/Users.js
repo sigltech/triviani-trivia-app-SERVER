bfc0f54deb253c01ad7efda7eddc360053b82b79
Aaron Smith <2021manifest@gmail.com>
2022-07-19 15:53:41 +0100

    added basic routes and controllers

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,1 +1,18 @@
-hj
+
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/User");
+
+
+//////////////////////////////////
+
+async function indexUsers(req, res) {
+    try {
+        const users = await User.all
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+module.exports = { indexUsers }
--- routes/Users.js
+++ routes/Users.js
@@ -1,1 +1,9 @@
-f
+
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/Users')
+
+router.get('/', userController.indexUsers)
+
+
+module.exports = router;
