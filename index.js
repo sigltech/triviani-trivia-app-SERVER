b443de003905eeaef710747d9c33ed7b8249dd29
Aaron Smith <2021manifest@gmail.com>
2022-07-19 15:37:08 +0100

    set up basic backend file system

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
 const express = require('express');
-const cors = require('cors');
-const { Server } = require('socket.io');
 const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 const socketPort = process.env.SOCKET_PORT || 8000;
+// const port = process.env.PORT || 3000
 
 const app = express();
 
 app.use(cors());
 
+
+// const userRoutes = require('./routes/users')
+
+// const leaderRoutes = require('./routes/leader')
+
+// app.use('/users', userRoutes)
+// app.use('/leaderboard', leaderRoutes)
+
+// app.get('/', (req, res) => res.send('Welcome to the library'))
+
+
+
+// socket.io below ///
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
-        methods: ['GET','POST','PUT','DELETE'],
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: 'Content-Type, Authorization, X-Requested-With, X-Socket-ID'
     }
 });
 
-server.listen(socketPort, () => {
-    console.log(`Server is running on port ${socketPort}`);
+io.on('connection', (socket) => {
+    console.log(`user connected: ${socket.id}`)
+
+    socket.on("join_room", data => {
+        socket.join(data)
+    })
+
+    socket.on("send_message", data => {
+        // console.log(data)
+        socket.to(data.room).emit("receive_message", data)
+    })
+
+
 })
+
+server.listen(socketPort, () => {
+    console.log(`Socket is running on port ${socketPort}`);
+})
+
+
+
+
+
+// app.listen(port, () => console.log(`Express now departing from port ${port}!`))
